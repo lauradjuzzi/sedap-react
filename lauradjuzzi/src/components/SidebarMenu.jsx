621e314de3487c3dd2534f7507e2528e724c0ca0
lauradjuzzi <laura23ti@mahasiswa.pcr.ac.id>
2025-06-05 08:40:08 +0700

    cloningannya

--- lauradjuzzi/src/components/SidebarMenu.jsx
+++ lauradjuzzi/src/components/SidebarMenu.jsx
@@ -1,5 +1,14 @@
-import { BiError } from "react-icons/bi"; 
-import { AiFillHome, AiOutlineUnorderedList, AiOutlineUser } from "react-icons/ai";
+import { BiError } from "react-icons/bi";
+import {
+  AiFillHome,
+  AiOutlineUnorderedList,
+  AiOutlineUser,
+} from "react-icons/ai";
+
+import {
+  MdFastfood,
+} from "react-icons/md";
+
 import { Link, NavLink } from "react-router-dom";
 
 export default function SidebarMenu() {
@@ -21,24 +30,32 @@ export default function SidebarMenu() {
 
       <li>
         <NavLink id="menu-2" to="/orders" className={menuClass}>
-        <AiOutlineUnorderedList className="mr-4 text-xl"/>
+          <AiOutlineUnorderedList className="mr-4 text-xl" />
           Orders
         </NavLink>
       </li>
 
       <li>
         <NavLink id="menu-3" to="/customers" className={menuClass}>
-        <AiOutlineUser className="mr-4 text-xl"/>
+          <AiOutlineUser className="mr-4 text-xl" />
           Customers
         </NavLink>
       </li>
 
       <li>
         <NavLink id="menu-4" to="/users" className={menuClass}>
-        <AiOutlineUser className="mr-4 text-xl"/>
+          <AiOutlineUser className="mr-4 text-xl" />
           User
         </NavLink>
       </li>
+
+      <li>
+        <NavLink id="menu-4" to="/products" className={menuClass}>
+          <MdFastfood className="mr-4 text-xl" />
+          Products
+        </NavLink>
+      </li>
+      
 
       {/* <li>
         <NavLink id="menu-4" to="/error400" className={menuClass}>
